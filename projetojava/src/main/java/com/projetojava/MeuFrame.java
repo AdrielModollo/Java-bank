607a1feba36231374a25c3240f19cc1c284b9e67
AdrielModollo <adrielmodollo@outlook.com>
2022-10-19 14:02:15 -0300

    feat: add button

--- projetojava/src/main/java/com/projetojava/MeuFrame.java
+++ projetojava/src/main/java/com/projetojava/MeuFrame.java
@@ -1,15 +1,20 @@
 package com.projetojava;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MeuFrame {
+public class MeuFrame extends JFrame {
 	
 	public MeuFrame() {
-		JFrame frame = new JFrame();
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Permite ao usuário fechar o frame assim que clicar no botão “X”
-		frame.setSize(300, 300); //Define o tamanho do frame (300 pixels de largura e 300 pixels de altura).
-		frame.setVisible(true); //Define o frame como visível (pode estar oculto).
+		super("Meu frame");
+		
+		JButton botao = new JButton("Clique");
+		
+		getContentPane().add(botao);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Permite ao usuário fechar o frame assim que clicar no botão “X”
+		setSize(300, 300); //Define o tamanho do frame (300 pixels de largura e 300 pixels de altura).
+		setVisible(true); //Define o frame como visível (pode estar oculto).
 	}
 
 	public static void main(String[] args) {
